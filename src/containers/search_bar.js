135b131b73b611337bb8f107a7d4dd57eabb0d9d
ibrahimovyusif <yusifibrahimov@hotmail.com>
2016-01-19 18:56:41 +0400

    weather forecast started, searchBar done

---
+++ src/containers/search_bar.js
@@ -0,0 +1,28 @@
+import React,  { Component } from 'react';
+
+export default class SearchBar extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { term: '' };
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) { 
+		this.setState({term: event.target.value})
+	}
+
+	render(){
+		return (
+			<form className="input-group">
+				<input 
+					placeholder='search and press the button'
+					className='form-control'
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
+				<button type="submit" className="btn btn-secondary">Submit</button>
+			</form>
+		)
+	}
+}
